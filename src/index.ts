e0869394f653ee19b3526c6c7decccb3e56e17f8
junehyung <jhcj910805@gmail.com>
2024-01-30 22:46:51 +0900

    Update button text in LitComponent

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export class LitComponent extends LitElement {
   render() {
     return html`
       <div class="button" @click=${() => console.log("hello")}>
-        ${this.display} Bye! sss
+        ${this.display} Bye! sss kkk
       </div>
     `;
   }
