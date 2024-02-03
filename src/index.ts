bb906fed987588f5c432b1e88a76a4bade35a15d
junehyung <jhcj910805@gmail.com>
2024-02-03 17:05:59 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export class LitComponent extends LitElement {
   render() {
     return html`
       <div class="button" @click=${() => console.log("hello")}>
-        ${this.display} cBye!
+        ${this.display} cBccye!
       </div>
     `;
   }
